47e8ed338972e73731436d7b5d114a7822c31fcf
35P10 <villanueva.guerrero@outlook.com>
2023-05-06 22:37:10 -0500

    feat: marker detection

--- main.cpp
+++ main.cpp
@@ -76,11 +76,10 @@ int main(int argc, char** argv ){
     //glMatrixMode(GL_MODELVIEW);    // Set the matrix mode to object modeling
 
 
-/*
     cv::Mat cameraMatrix, distCoeffs;
     float markerLength = 0.05;
     
-    // You can read camera parameters from tutorial_camera_params.yml
+    // Read camera parameters from tutorial_camera_params.yml
     readCameraParameters("D:/Documents/Projects/openCV/camera_parameters.yml", cameraMatrix, distCoeffs); // This function is implemented in aruco_samples_utility.hpp
     
     // Set coordinate system
@@ -93,7 +92,6 @@ int main(int argc, char** argv ){
     cv::aruco::DetectorParameters detectorParams = cv::aruco::DetectorParameters();
     cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
     aruco::ArucoDetector detector(dictionary, detectorParams);
-*/
 
     while(!glfwWindowShouldClose(window))
     {
@@ -102,20 +100,42 @@ int main(int argc, char** argv ){
         processInput(window);
         // render
         // ------   
+        cv::Mat frame_input, frame_output;
+        inputVideo.retrieve(frame_input);
+        frame_input.copyTo(frame_output);
 
-        cv::Mat frame;
-        inputVideo.read(frame);
-        flip(frame, frame, 0); // flip loaded frame on the y-axis.
+        std::vector<int> ids;
+        std::vector<std::vector<cv::Point2f>> corners;
+
+        detector.detectMarkers(frame_input, corners, ids);
+
+        if (ids.size() > 0) {
+            cv::aruco::drawDetectedMarkers(frame_output, corners, ids);
+            int nMarkers = corners.size();
+            std::vector<cv::Vec3d> rvecs(nMarkers), tvecs(nMarkers);
+            // Calculate pose for each marker
+            for (int i = 0; i < nMarkers; i++) {
+                solvePnP(objPoints, corners.at(i), cameraMatrix, distCoeffs, rvecs.at(i), tvecs.at(i));
+            }
+            // Draw axis for each marker
+            for(unsigned int i = 0; i < ids.size(); i++) {
+                cv::drawFrameAxes(frame_output, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1);
+            }
+        }
+
+
+        flip(frame_output, frame_output, 0); // flip loaded frame on the y-axis.
+
         
         /* OpenGL texture binding of the image loaded by DevIL  */
         unsigned int texture;
         glGenTextures(1, &texture);
         glBindTexture(GL_TEXTURE_2D, texture);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, frame.cols, frame.rows, 0, GL_BGR, GL_UNSIGNED_BYTE, frame.data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, frame_output.cols, frame_output.rows, 0, GL_BGR, GL_UNSIGNED_BYTE, frame_output.data);
         glGenerateMipmap(GL_TEXTURE_2D);
         
         //stbi_set_flip_vertically_on_load(true); // doesnt work!! tell stb_image.h to flip loaded texture's on the y-axis.
-/*
+        /*
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f); 
         glClearDepth(0.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the window
@@ -124,7 +144,8 @@ int main(int argc, char** argv ){
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D,texture);
         glMatrixMode(GL_MODELVIEW);     // Operate on model-view matrix
-*/
+        */
+       
         glClear(GL_COLOR_BUFFER_BIT);
         // Draw texture in GLFW window
         glBegin(GL_QUADS);
@@ -143,40 +164,6 @@ int main(int argc, char** argv ){
         glDeleteTextures(1, &texture);
     }
 
-/*
-    while (inputVideo.grab()) {
-        //Lee una imagen de la cámara
-        // Convierte la imagen a escala de grises
-        // Detecta los marcadores Aruco en la imagen
-        // Dibuja los marcadores detectados en la imagen
-        // Muestra la imagen en una ventana
-        cv::Mat image, imageCopy;
-        inputVideo.retrieve(image);
-        image.copyTo(imageCopy);
-        std::vector<int> ids;
-        std::vector<std::vector<cv::Point2f>> corners;
-        detector.detectMarkers(image, corners, ids);
-        // If at least one marker detected
-        if (ids.size() > 0) {
-            cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
-            int nMarkers = corners.size();
-            std::vector<cv::Vec3d> rvecs(nMarkers), tvecs(nMarkers);
-            // Calculate pose for each marker
-            for (int i = 0; i < nMarkers; i++) {
-                solvePnP(objPoints, corners.at(i), cameraMatrix, distCoeffs, rvecs.at(i), tvecs.at(i));
-            }
-            // Draw axis for each marker
-            for(unsigned int i = 0; i < ids.size(); i++) {
-                cv::drawFrameAxes(imageCopy, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.1);
-            }
-        }
-        // Show resulting image and close window
-        cv::imshow("out", imageCopy);
-        char key = (char) cv::waitKey(1);
-        if (key == 27)
-            break;
-    }
-*/
     inputVideo.release();
     glfwTerminate();
 
